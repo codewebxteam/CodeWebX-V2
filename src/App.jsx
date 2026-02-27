538e1f3d77e64942656db8126dcb78ab7ada4904
Aditya Kr <gadityakumar77@gmail.com>
2026-02-27 21:22:20 +0530

    Upfaye

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import AboutSection from "./components/AboutSection";
+import Navbar from "./components/Common/Navbar";
+import Home from "./pages/Home";
 
+/**
+ * Main App component.
+ * It provides the global layout and renders the Home page content.
+ */
 function App() {
   return (
     <div className="relative w-full bg-black">
+      {/* Fixed Navbar appears across all pages */}
       <Navbar />
-      <Hero />
-      <AboutSection />
+
+      {/* Main content area */}
+      <main>
+        <Home />
+      </main>
+
+      {/* Background radial glow for a premium aesthetic */}
+      <div className="fixed top-[-10%] left-[-10%] w-[40%] h-[40%] bg-zinc-900/20 blur-[120px] rounded-full pointer-events-none -z-10"></div>
     </div>
   );
 }
